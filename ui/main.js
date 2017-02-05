aee2161380feb497f638e59b18d67e52dc34cfb6
BISHALSARKAR <bishalsarkar166@gmail.com>
2017-02-05 16:53:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 //move the image
 var img=document.getElementById('madi');
-var marginLeft=200;
+var marginLeft=0;
 function moveRight(){
     
     
