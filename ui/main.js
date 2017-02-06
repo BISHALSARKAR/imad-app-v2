7eabb4ad046fb62cccd6375451caf7ef0cf5e0ea
BISHALSARKAR <bishalsarkar166@gmail.com>
2017-02-06 17:07:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,27 +10,14 @@ button.onclick=function(){
        if(request.status==200)
        {
             //capture a list of names and render it as a list
-    var names=request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-   var ul=document.getElementById('namelist');
-   ul.innerHTML = list;
-    
+    var counter=request.responseText;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
        }
-       
+           
        }
-       //Not done yet
    };
-   //Make the request
-   request.open('GET','http://bishalsarkar.imad.hasura-app.io/submit-name' + name,true);
-   request.send(null);
-  
-};//submit name
-
-var submit=document.getElementById('submit_btn');
+   
 submit.onclick=function(){
     //Create a request object
    var request=new XMLHttpRequest();
